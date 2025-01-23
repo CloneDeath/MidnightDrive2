31ae5dbcaf3558ab0b43dbf9e647065d21126d67
Nicholas Rodine <halfofastaple@gmail.com>
2025-01-23 05:52:40 -0500

    added screen space pixelize shader

---
+++ effects/pixelize_screen.gd
@@ -0,0 +1,16 @@
+@tool
+extends MeshInstance3D
+
+@export var resolution: Vector2 = Vector2(10, 10) :
+	get:
+		return resolution;
+	set(value):
+		resolution = value;
+		update_shader_parameters();
+
+func _ready() -> void:
+	update_shader_parameters();
+
+func update_shader_parameters():
+	var material: ShaderMaterial = self.get_active_material(0);
+	material.set_shader_parameter("resolution", resolution);
